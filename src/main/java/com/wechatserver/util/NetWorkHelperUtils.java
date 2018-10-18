5f67eaffce06b0159a8c786479736af8b84b7f8c
user <user@user-desktop>
2018-10-18 15:55:25 +0800

    20181018

--- src/main/java/com/wechatserver/util/NetWorkHelperUtils.java
+++ src/main/java/com/wechatserver/util/NetWorkHelperUtils.java
@@ -75,19 +75,17 @@ public class NetWorkHelperUtils {
 
 		@Override
 		public X509Certificate[] getAcceptedIssuers() {
-			// TODO Auto-generated method stub
 			return null;
 		}
 
 		@Override
 		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-			// TODO Auto-generated method stub
 
 		}
 
 		@Override
 		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-			// TODO Auto-generated method stub
+
 
 		}
 	};
